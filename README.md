e4d904fb4eb50e242a457ee96e271f5a3c887ef4
salcabar <salcabar@126.com>
2015-10-09 15:07:47 +0800

    添加日志文件

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # desktoppr
 下载desktoppr图集的
+
+新建一个images文件夹用来存图，新建一个logs文件夹用来存日志
--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 var fs = require('fs');
 var path = require('path');
-var Q = require('q');
 var https = require('https');
 var request = require('request');
-var bufferHelper = require('bufferhelper');
+var BufferHelper = require('bufferhelper');
 var iconv = require('iconv-lite');
+var log = require('./logHelp').helper;
 
 var url = "https://api.desktoppr.co/1/wallpapers";
 
-var bufferHelper = new bufferHelper();
+getPages(url, 1, 100);
 
-var deferred = Q.defer();
+function getPages(url, beginPageId, endPageId) {
+    
+    var bufferHelper = new BufferHelper();
+    var data = null;
+    var pageUrl = url + '?page=' + beginPageId;
 
-var data = null;
+    log.info('pageUrl------ ' + pageUrl);
 
+    https.get(pageUrl, function(res) {
 
-https.get(url, function(res) {
+        res.on('data', function(chunk){
 
-    res.on('data', function(chunk){
+          bufferHelper.concat(chunk);
 
-      bufferHelper.concat(chunk);
+        });
 
-    });
+        res.on('end', function(){
 
-    res.on('end', function(){
+            data = iconv.decode(bufferHelper.toBuffer(),'gb2312');
+            
+            var obj = JSON.parse(data);
+            var dataList = obj.response;
+            var length = dataList.length;
+            var flag = 0;
+            
+            dataList.forEach(function(data,i){
 
-        data = iconv.decode(bufferHelper.toBuffer(),'gb2312');
-        
-        var obj = JSON.parse(data);
-        var dataList = obj.response;
+                var filename = parseUrlForFileName(data.image.url);
+                var imageUrl = data.image.url;
 
-        dataList.forEach(function(data,i){
-            var filename = parseUrlForFileName(data.image.url);
-            var url = data.image.url;
-            downloadImg(url, filename, function() {
-                console.log(filename + ' done');
+                downloadImg(imageUrl, filename, function() {
+                    flag++;
+                    log.info('第' + beginPageId + '页' + '----第' + i + '张完成',flag);
+                    if(length == flag) {
+                        log.warn('第' + beginPageId + '页完成');
+                        if(beginPageId == endPageId) return;
+                        getPages(url , beginPageId + 1, endPageId);
+                    }
+                })
             })
-        })
-      
-      deferred.resolve(data);
 
-      
+          
+        });
+
+    }).on('error', function(e) {
+
     });
-
-}).on('error', function(e) {
-
-  deferred.reject();
-
-});
+}
 
 function parseUrlForFileName(address) {
     var filename = path.basename(address);
@@ -56,14 +66,13 @@ function parseUrlForFileName(address) {
 
 var downloadImg = function(uri, filename, callback){
     request.head(uri, function(err, res, body){
-    // console.log('content-type:', res.headers['content-type']);  //这里返回图片的类型
-    // console.log('content-length:', res.headers['content-length']);  //图片大小
-    if (err) {
-        console.log('err: '+ err);
-        return false;
-    }
-    console.log('res: '+ res);
-    request(uri).pipe(fs.createWriteStream('images/'+filename)).on('close', callback);  //调用request的管道来下载到 images文件夹下
+        log.info('content-type:', res.headers['content-type']);  //这里返回图片的类型
+        log.info('content-length:', Math.round(res.headers['content-length'] / 1024) + 'KB');  //图片大小
+        if (err) {
+            log.error('err: '+ err);
+            return false;
+        }
+        request(uri).pipe(fs.createWriteStream('images/'+filename)).on('close', callback);  
     });
 };
  
